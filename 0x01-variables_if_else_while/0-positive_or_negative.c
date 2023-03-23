cc15eff071f06feda433160113915e5299215498
ABOULHAJ <aboulhaj@student.1337.ma>
2023-03-23 04:13:25 -0700

    adding description for function main

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	// check if the number is positive
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
+	// ckeck if the number is negative
 	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
+	// check if the number is 0
 	else
 	{
 		printf("%d is zero\n", n);
