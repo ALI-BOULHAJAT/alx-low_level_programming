ab82adcd1ffed825d40f74a5a6295205304c2876
ABOULHAJ <aboulhaj@student.1337.ma>
2023-03-23 04:37:42 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,24 +5,22 @@
 
 /* description for main function */
 /* Program assign a random number and detect */
-/*if the number is negative or positive or null */
+/* if the number is negative or positive or null */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* check if the number is positive */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	/* ckeck if the number is negative */
 	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
-	/* check if the number is 0 */
 	else
 	{
 		printf("%d is zero\n", n);
